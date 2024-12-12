36cb5cdd421eda3a80f7b8210ddf7c23030c8aeb
Lokesh R <115632121+lokesh-131-R@users.noreply.github.com>
2024-12-12 20:23:11 +0530

    Update Chat_with_your_data.py

--- Chat_with_your_data.py
+++ Chat_with_your_data.py
@@ -143,12 +143,7 @@ if "messages" not in st.session_state:
 with columns[1]:
     with st.container(border=True):
         response_container = st.container(height=450)
-        selected_df = st.selectbox(
-            "Select Topic to chat with",
-            list(agents.keys()),
-            format_func=lambda x: f'<div style="font-size: small;">{x}</div>',
-            unsafe_allow_html=True
-        )
+        selected_df = st.selectbox("Select Topic to chat with", list(agents.keys()))
         if prompt := st.chat_input("Ask your question here"):
             st.session_state.messages.append({"role": "user", "content": prompt})
             conversation_history = "\n".join([msg["content"] for msg in st.session_state.messages])
